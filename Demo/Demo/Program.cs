8da1840e54a5a8fbdd7eac0f4db59d2985dd6721
jpdemory <jpdemory@manastria.fr>
2018-01-26 14:57:43 +0100

    Table des 8 : affichage simple

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Afficher la tables des 8
+            // Concaténation : +
+            // Saut de ligne : \n
+            const int multiple = 8;
+            const int valMax = 10;
+
+            string resultat = "";
+
+            for (int i=0 ; i<=valMax ; i++)
+            {
+                resultat = resultat + i + " x " + multiple;
+                resultat += " = " + (i * multiple);
+                resultat += "\n";
+            }
+
+            Console.WriteLine(resultat);
+
+            Console.WriteLine("Press a key");
+            Console.ReadKey();
+        }
+    }
+}
