101faf790dd288ed1815c188fb8dd1a531030561
jpdemory <jpdemory@manastria.fr>
2018-02-02 14:58:38 +0100

    Ajout de la classe LigneCmd, création de 2 objets

---
+++ Commandes/Commandes/LigneCmd.cs
@@ -0,0 +1,20 @@
+﻿namespace Commandes
+{
+    class LigneCmd // nom complet : Commandes.LigneCmd
+    {
+        public string designation;
+        public double prixUnitaire;
+        public double quantite;
+
+        public double Montant()
+        {
+            return quantite * prixUnitaire;
+        }
+
+        public string Description()
+        {
+            return
+                $"{designation}, qt:{quantite}, pu:{prixUnitaire:C}, mnt:{Montant():C}";
+        }
+    }
+}
---
+++ Commandes/Commandes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace Commandes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            // Création d'un premier objet LigneCmd
+            // l1 est une référence
+            LigneCmd l1;
+
+            // Création d'un objet LigneCmd référencé par l1
+            l1 = new LigneCmd();
+
+            // Modification de la désignation
+            l1.designation = "Gomme";
+            l1.prixUnitaire = 2.1;
+            l1.quantite = 10;
+
+            LigneCmd l2 = new LigneCmd()
+            {
+                designation = "Crayon",
+                prixUnitaire = 0.2,
+                quantite = 5
+            };
+
+            Console.WriteLine(l1.Description());
+            Console.WriteLine(l2.Description());
+
+            Console.WriteLine("Press a key");
+            Console.ReadKey();
+        }
+    }
+}
